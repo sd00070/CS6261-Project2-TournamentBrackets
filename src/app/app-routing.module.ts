df8b178d3bd1f9ecb3df757f3fb803b3aade0039
Spencer Dent <sd00070@my.westga.edu>
2021-06-21 04:40:45 -0400

    configure router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BracketsComponent } from './tournament/brackets/brackets.component';
+import { HelloComponent } from './tournament/hello/hello.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
+  { path: 'hello', component: HelloComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'brackets', component: BracketsComponent },
+  { path: '**', redirectTo: '/hello' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
