e1e0c31bbaec53c91d2a200faa82dea30c13e35c
Spencer Dent <sd00070@my.westga.edu>
2021-06-21 10:41:09 -0400

    create & test helper methods

--- src/app/services/roster.service.spec.ts
+++ src/app/services/roster.service.spec.ts
@@ -10,39 +10,113 @@ describe('RosterService', () => {
     rosterService = TestBed.inject(RosterService)
   })
 
-  it('should not allow duplicate names', () => {
-    rosterService.addContestant('Bill')
-    expect(() => { rosterService.addContestant('Bill') }).toThrow(new Error('Cannot add duplicate name'))
+  describe('addContestant', () => {
+    it('should not allow duplicate names', () => {
+      rosterService.addContestant('Bill')
+      expect(() => { rosterService.addContestant('Bill') }).toThrow(new Error('Cannot add duplicate name'))
+    })
+
+    it('should not allow null names', () => {
+      expect(() => { rosterService.addContestant(null) }).toThrow(new Error('Name should not be null'))
+    })
+
+    it('should not allow empty string names', () => {
+      expect(() => { rosterService.addContestant('') }).toThrow(new Error('Name should not be empty'))
+    })
+
+    it('should add one contestant', () => {
+      expect(rosterService.getContestants().length).toBe(0)
+
+      rosterService.addContestant('Fred')
+      expect(rosterService.getContestants().length).toBe(1)
+      expect(rosterService.getContestants()[0]).toEqual('Fred')
+    })
+
+    it('should add several contestants', () => {
+      expect(rosterService.getContestants().length).toBe(0)
+
+      rosterService.addContestant('Jesse')
+      rosterService.addContestant('James')
+      rosterService.addContestant('Bruce')
+      rosterService.addContestant('Cassidy')
+
+      expect(rosterService.getContestants().length).toBe(4)
+      expect(rosterService.getContestants()[0]).toEqual('Jesse')
+      expect(rosterService.getContestants()[1]).toEqual('James')
+      expect(rosterService.getContestants()[2]).toEqual('Bruce')
+      expect(rosterService.getContestants()[3]).toEqual('Cassidy')
+    })
   })
 
-  it('should not allow null names', () => {
-    expect(() => { rosterService.addContestant(null) }).toThrow(new Error('Name should not be null'))
+
+  describe('hasContestants', () => {
+    it('should return false when the array is empty', () => {
+      expect(rosterService.hasContestants()).toBeFalse()
+    })
+
+    it('should return true when the array contains values', () => {
+      rosterService.addContestant('Jack')
+      rosterService.addContestant('Kate')
+      expect(rosterService.hasContestants()).toBeTrue()
+    })
   })
 
-  it('should not allow empty string names', () => {
-    expect(() => { rosterService.addContestant('') }).toThrow(new Error('Name should not be empty'))
+  describe('static containsString', () => {
+    it('should return false if the array is empty', () => {
+      expect(RosterService.containsString('hello', [])).toBeFalse()
+    })
+
+    it('should return false if the string is not in the array', () => {
+      expect(RosterService.containsString('hello', ['world', '!'])).toBeFalse()
+    })
+
+    it('should return true if the string is in the array', () => {
+      expect(RosterService.containsString('hello', ['hello', 'world', '!'])).toBeTrue()
+    })
   })
 
-  it('should add one contestant', () => {
-    expect(rosterService.getContestants().length).toBe(0)
+  describe('containsContestant', () => {
+    it('should return false if the contestant list is empty', () => {
+      expect(rosterService.containsContestant('William')).toBeFalse()
+    })
 
-    rosterService.addContestant('Fred')
-    expect(rosterService.getContestants().length).toBe(1)
-    expect(rosterService.getContestants()[0]).toEqual('Fred')
+    it('should return false if the list does not contain the given name', () => {
+      rosterService.addContestant('Nathan')
+      expect(rosterService.containsContestant('Michael')).toBeFalse()
+    })
+
+    it('should return true if it contains the contestant', () => {
+      rosterService.addContestant('Jeff')
+      expect(rosterService.containsContestant('Jeff')).toBeTrue()
+    })
   })
 
-  it('should add several contestants', () => {
-    expect(rosterService.getContestants().length).toBe(0)
+  describe('isValidPlayer', () => {
+    it('should return false if the given player is null', () => {
+      expect(rosterService.isValidPlayer(null)).toBeFalse()
+    })
 
-    rosterService.addContestant('Jesse')
-    rosterService.addContestant('James')
-    rosterService.addContestant('Bruce')
-    rosterService.addContestant('Cassidy')
+    it('should return false if the given value is an empty string', () => {
+      expect(rosterService.isValidPlayer('')).toBeFalse()
+    })
 
-    expect(rosterService.getContestants().length).toBe(4)
-    expect(rosterService.getContestants()[0]).toEqual('Jesse')
-    expect(rosterService.getContestants()[1]).toEqual('James')
-    expect(rosterService.getContestants()[2]).toEqual('Bruce')
-    expect(rosterService.getContestants()[3]).toEqual('Cassidy')
+    it('should return false if the name is already in the list', () => {
+      rosterService.addContestant('Darryl')
+      expect(rosterService.isValidPlayer('Darryl')).toBeFalse()
+    })
+
+    it('should return true if and only if the value is a non-null/-empty string not in the list', () => {
+      rosterService.addContestant('Tarzan')
+      expect(rosterService.isValidPlayer('Jane')).toBeTrue()
+    })
+  })
+
+  describe('clearRoster', () => {
+    it('should set the roster to an empty array', () => {
+      rosterService.addContestant('Sonic')
+      rosterService.addContestant('Elise')
+      rosterService.clearRoster()
+      expect(rosterService.getContestants()).toEqual([])
+    })
   })
 })
--- src/app/services/roster.service.ts
+++ src/app/services/roster.service.ts
@@ -13,13 +13,35 @@ export class RosterService {
     return this.contestants
   }
 
+  hasContestants(): boolean {
+    return this.contestants.length > 0
+  }
+
+  static containsString(string: string, array: string[]): boolean {
+    return array.some((element: string) => element === string)
+  }
+
+  containsContestant(playerName: string): boolean {
+    return RosterService.containsString(playerName, this.contestants)
+  }
+
   addContestant(player: string | null) {
     if (player === null) throw new Error('Name should not be null')
 
     if (player === '') throw new Error('Name should not be empty')
 
-    if (this.contestants.find((contestant: string) => player === contestant)) throw new Error('Cannot add duplicate name')
+    if (this.containsContestant(player)) throw new Error('Cannot add duplicate name')
 
     this.contestants.push(player)
+  }
+
+  isValidPlayer(player: string | null): boolean {
+    return player !== null
+      && player !== ''
+      && !this.containsContestant(player)
+  }
+
+  clearRoster(): void {
+    this.contestants = []
   }
 }
