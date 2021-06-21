3aa7ba3e5ff119c60fd5f9dd3648e09d4b6096ca
Spencer Dent <sd00070@my.westga.edu>
2021-06-21 04:00:40 -0400

    config karma, serve, & test output

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,11 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-spec-reporter')
     ],
     client: {
       jasmine: {
@@ -32,13 +33,19 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    autoWatch: true,  // TODO: remember to flip me before submission
+    browsers: ['FirefoxHeadless'],
+    customLaunchers: {
+      'FirefoxHeadless': {
+        base: 'Firefox',
+        flags: ['-headless',],
+      }
+    },
+    singleRun: false, // TODO: remember to flip me before submission
     restartOnFileChange: true
   });
 };
