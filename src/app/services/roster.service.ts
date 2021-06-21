2c2fd6abba312b933b4edde304262bddd477a062
Spencer Dent <sd00070@my.westga.edu>
2021-06-21 12:11:43 -0400

    add error-throwing methods & remove unused methods

--- src/app/services/roster.service.ts
+++ src/app/services/roster.service.ts
@@ -21,24 +21,27 @@ export class RosterService {
     return array.some((element: string) => element === string)
   }
 
-  containsContestant(playerName: string): boolean {
-    return RosterService.containsString(playerName, this.contestants)
+  addContestant(player: string | null) {
+    RosterService.throwErrorIfPlayerIsInvalid(player, this.contestants)
+
+    // typescript is making me check this again even though I know it cannot be null
+    if (player) this.contestants.push(player)
   }
 
-  addContestant(player: string | null) {
+  static throwErrorIfPlayerIsInvalid(player: string | null, playerArray: string[]): void {
     if (player === null) throw new Error('Name should not be null')
 
     if (player === '') throw new Error('Name should not be empty')
 
-    if (this.containsContestant(player)) throw new Error('Cannot add duplicate name')
-
-    this.contestants.push(player)
+    if (RosterService.containsString(player, playerArray)) throw new Error('Cannot add duplicate name')
   }
 
-  isValidPlayer(player: string | null): boolean {
-    return player !== null
-      && player !== ''
-      && !this.containsContestant(player)
+  static throwErrorIfRosterIsInvalidLength(roster: string[]) {
+    if (
+      roster.length !== 2
+      && roster.length !== 4
+      && roster.length !== 8
+    ) throw new Error('Must contain 2, 4, or 8 players')
   }
 
   clearRoster(): void {
