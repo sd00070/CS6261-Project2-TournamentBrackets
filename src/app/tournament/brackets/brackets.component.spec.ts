5e842ededcced3c89b30c3a0aced6ad2d35949dd
Spencer Dent <sd00070@my.westga.edu>
2021-06-22 16:47:19 -0400

    test completeRound

--- src/app/tournament/brackets/brackets.component.spec.ts
+++ src/app/tournament/brackets/brackets.component.spec.ts
@@ -36,6 +36,10 @@ describe('BracketsComponent', () => {
     fixture.detectChanges()
   })
 
+  it('should start on round 1', () => {
+    expect(component.roundNumber).toBe(1)
+  })
+
   describe('onInit', () => {
     it('should set matches to the partitioned roster', () => {
       expect(component.matches).toEqual([new Match('one', 'two')])
@@ -51,6 +55,47 @@ describe('BracketsComponent', () => {
     it('should return false if the matches list is empty', () => {
       component.matches = []
       expect(component.hasMatches()).toBeFalse()
+    })
+  })
+
+  describe('completeRound', () => {
+    it('should increment the round number if there are more than one match', () => {
+      component.matches = [new Match('one', 'two'), new Match('three', 'four')]
+      component.completeRound()
+      expect(component.roundNumber).toBe(2)
+    })
+
+    it('should half the number of matches', () => {
+      component.matches = [new Match('a', 'b'), new Match('c', 'd')]
+      component.completeRound()
+      expect(component.matches.length).toBe(1)
+
+      component.matches = [new Match('a', 'b'), new Match('c', 'd'), new Match('e', 'f'), new Match('g', 'h')]
+      component.completeRound()
+      expect(component.matches.length).toBe(2)
+    })
+
+    it('should set the winner if and only if there is only one match', () => {
+      let testMatch = new Match('a', 'b')
+
+      component.matches = [testMatch]
+      component.completeRound()
+      expect(component.winner).toEqual(testMatch.winner)
+
+      component.winner = ''
+      component.matches = [testMatch, new Match('c', 'd')]
+      component.completeRound()
+      expect(component.winner).toEqual('')
+    })
+
+    it('should set matches to a new set of matches from the winners', () => {
+      let match1 = new Match('a', 'b')
+      let match2 = new Match('c', 'd')
+
+      component.matches = [match1, match2]
+
+      component.completeRound()
+      expect(component.matches).toEqual([new Match(match1.winner, match2.winner)])
     })
   })
 })
--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -25,7 +25,6 @@ export class BracketsComponent implements OnInit {
     this.matches = Match.partition(this.rosterService.getContestants())
   }
 
-  // TODO: test this method
   completeRound(): void {
     let winners: string[] = this.matches.map((match: Match) => match.winner)
 
