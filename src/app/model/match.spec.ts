e6b152ca7a09e657215e09752a67dc29de753fb0
Spencer Dent <sd00070@my.westga.edu>
2021-06-22 14:43:33 -0400

    test & add setters & getters

---
+++ src/app/model/match.spec.ts
@@ -0,0 +1,68 @@
+import { Match } from './match'
+
+describe('Match', () => {
+  let testMatch: Match
+
+  beforeEach(() => {
+    testMatch = new Match('player1', 'player2')
+  })
+
+  describe('player1 setter & getter', () => {
+    it('should set the Match\'s player1 to the given value', () => {
+      testMatch.player1 = 'Sun Tzu'
+      expect(testMatch.player1).toEqual('Sun Tzu')
+    })
+
+    it('should throw an error if passed an empty string', () => {
+      expect(() => { testMatch.player1 = '' }).toThrow(new Error('cannot set player1 to empty string'))
+    })
+
+    it('if the winner is set to player1, changes the winner as well', () => {
+      testMatch.winner = testMatch.player1
+      testMatch.player1 = 'Sun Tzu'
+      expect(testMatch.winner).toEqual('Sun Tzu')
+    })
+  })
+
+  describe('player2 setter & getter', () => {
+    it('should set Match\'s player2 to the given value', () => {
+      testMatch.player2 = 'Wumen Huikai'
+      expect(testMatch.player2).toEqual('Wumen Huikai')
+    })
+
+    it('should throw an error if passed an empty string', () => {
+      expect(() => { testMatch.player2 = '' }).toThrow(new Error('cannot set player2 to empty string'))
+    })
+
+    it('if the winner is set to player2, changes the winner as well', () => {
+      testMatch.winner = testMatch.player2
+      testMatch.player2 = 'Wumen Huikai'
+      expect(testMatch.winner).toEqual('Wumen Huikai')
+    })
+  })
+
+  describe('winner setter & getter', () => {
+    it('should set Match\'s winner to the given value', () => {
+      testMatch.winner = 'Dom Cobb'
+      expect(testMatch.winner).toEqual('Dom Cobb')
+    })
+
+    it('should throw an error if passed an empty string', () => {
+      expect(() => { testMatch.winner = '' }).toThrow(new Error('cannot set winner to empty string'))
+    })
+  })
+
+  describe('static partition', () => {
+    it('when passed 2 players, should create an array with a Match from those players', () => {
+      expect(Match.partition(['Luke', 'Han'])).toEqual([new Match('Luke', 'Han')])
+    })
+
+    it('when passed 4 players, should create an array of 2 Matches from those players', () => {
+      expect(Match.partition(['Mike', 'Don', 'Leo', 'Ralfie'])).toEqual([new Match('Mike', 'Don'), new Match('Leo', 'Ralfie')])
+    })
+
+    it('should throw an error if the number of players is not even', () => {
+      expect(() => Match.partition(['Ed', 'Edd', 'Eddy'])).toThrow(new Error('Player roster must contain an even number of players'))
+    })
+  })
+})
--- src/app/model/match.ts
+++ src/app/model/match.ts
@@ -1,14 +1,44 @@
 export class Match {
-    public winner: string
+    private _winner: string
 
     constructor(
-        public player1: string,
-        public player2: string,
+        private _player1: string,
+        private _player2: string,
     ) {
-        this.winner = this.player1
+        this._winner = this._player1
+    }
+
+    get player1(): string { return this._player1 }
+
+    set player1(newPlayer1: string) {
+        if (newPlayer1 === '') throw new Error('cannot set player1 to empty string')
+
+        if (this._winner === this._player1) this._winner = newPlayer1
+
+        this._player1 = newPlayer1
+    }
+
+    get player2(): string { return this._player2 }
+
+    set player2(newPlayer2: string) {
+        if (newPlayer2 === '') throw new Error('cannot set player2 to empty string')
+
+        if (this._winner === this._player2) this._winner = newPlayer2
+
+        this._player2 = newPlayer2
+    }
+
+    get winner(): string { return this._winner }
+
+    set winner(newWinner: string) {
+        if (newWinner === '') throw new Error('cannot set winner to empty string')
+
+        this._winner = newWinner
     }
 
     static partition(roster: string[]): Match[] {
+        if (roster.length % 2 === 1) throw new Error('Player roster must contain an even number of players')
+
         let tempRoster = [...roster]
         let matches: Match[] = []
         while (tempRoster.length > 0) {
